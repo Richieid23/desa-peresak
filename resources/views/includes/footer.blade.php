aeca6aa7be0d82acf6aa973c09c5a5ae5d979a3d
Richieid23 <fitrarizki23@gmail.com>
2021-08-01 11:23:09 +0700

    fixing footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,50 +1,40 @@
-<footer id="footer" class="border-0" style="background: url('img/parallax/parallax-8.jpg'); background-size:cover; background-position: 0 100%;">
-    <div class="container py-4">
-        <div class="row justify-content-md-center py-5 mt-3">
-            <div class="col-md-12 col-lg-3 d-flex align-items-center justify-content-center justify-content-lg-start mb-5 mb-lg-0">
-                <a href="index.html"><img src="img/logo-flat-light.png" alt="Logo" class="img-fluid" style="max-width: 100px;"></a>
+<footer id="footer" class="mt-0">
+    <div class="container my-4">
+        <div class="row py-5">
+            <div class="col-md-6 col-lg-3 mb-5 mb-lg-0">
+                <h5 class="text-5 text-transform-none font-weight-semibold text-color-light mb-4">Contact Details</h5>
+                <p class="text-4 mb-0">Porto Template 123</p>
+                <p class="text-4 mb-0">Porto Blvd, Suite</p>
+                <p class="text-4 mb-0">New York</p>
             </div>
-            <div class="col-md-6 col-lg-2 text-center text-lg-left mb-5 mb-lg-0">
-                <h5 class="text-5 text-transform-none font-weight-semibold text-color-light mb-4">Pages</h5>
-                <ul class="list list-unstyled">
-                    <li class="mb-1"><a href="page-services.html" class="link-hover-style-1"> Our Services</a></li>
-                    <li class="mb-1"><a href="about-us.html" class="link-hover-style-1"> About Us</a></li>
-                    <li class="mb-1"><a href="contact-us.html" class="link-hover-style-1"> Contact Us</a></li>
+            <div class="col-md-6 col-lg-3 mb-5 mb-lg-0">
+                <h5 class="text-5 text-transform-none font-weight-semibold text-color-light mb-4">Opening Hours</h5>
+                <p class="text-4 mb-0">Mon-Fri: <span class="text-color-light">8:30 am to 5:00 pm</span></p>
+                <p class="text-4 mb-0">Saturday: <span class="text-color-light">9:30 am to 1:00 pm</span></p>
+                <p class="text-4 mb-0">Sunday: <span class="text-color-light">Closed</span></p>
+            </div>
+            <div class="col-md-6 col-lg-3 mb-5 mb-lg-0">
+                <h5 class="text-5 text-transform-none font-weight-semibold text-color-light mb-4">Call Us Now</h5>
+                <p class="text-7 text-color-light font-weight-bold mb-2">(800) 123 4567</p>
+                <p class="text-4 mb-0">Sales: <span class="text-color-light">(800) 123 4568</span></p>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <h5 class="text-5 text-transform-none font-weight-semibold text-color-light mb-4">Social Media</h5>
+                <ul class="footer-social-icons social-icons m-0">
+                    <li class="social-icons-facebook"><a href="http://www.facebook.com/" target="_blank" title="Facebook"><i class="fab fa-facebook-f text-2"></i></a></li>
+                    <li class="social-icons-twitter"><a href="http://www.twitter.com/" target="_blank" title="Twitter"><i class="fab fa-twitter text-2"></i></a></li>
+                    <li class="social-icons-linkedin"><a href="http://www.linkedin.com/" target="_blank" title="Linkedin"><i class="fab fa-linkedin-in text-2"></i></a></li>
                 </ul>
-            </div>
-            <div class="col-md-6 col-lg-2 text-center text-lg-left mb-5 mb-lg-0">
-                <h5 class="text-5 text-transform-none font-weight-semibold text-color-light mb-4">Links</h5>
-                <ul class="list list-unstyled">
-                    <li class="mb-1"><a href="page-faq.html" class="link-hover-style-1"> FAQ's</a></li>
-                    <li class="mb-1"><a href="sitemap.html" class="link-hover-style-1"> Sitemap</a></li>
-                </ul>
-            </div>
-            <div class="col-md-6 col-lg-5 text-center text-lg-left">
-                <h5 class="text-5 text-transform-none font-weight-semibold text-color-light mb-4">Newsletter</h5>
-                <div class="alert alert-success d-none" id="newsletterSuccess">
-                    <strong>Success!</strong> You've been added to our email list.
-                </div>
-                <div class="alert alert-danger d-none" id="newsletterError"></div>
-                <form id="newsletterForm" action="php/newsletter-subscribe.php" method="POST" class="mb-3 mb-md-0">
-                    <div class="input-group input-group-rounded">
-                        <input class="form-control form-control-sm bg-light" placeholder="Email Address" name="newsletterEmail" id="newsletterEmail" type="text">
-                        <span class="input-group-append">
-                            <button class="btn btn-light text-color-dark" type="submit"><strong>GO!</strong></button>
-                        </span>
-                    </div>
-                </form>
-                <p class="mt-3 mb-0 text-center text-lg-left">
-                    <i class="fab fa-whatsapp text-color-primary"></i><span class="text-color-light opacity-7 pl-2">(800) 123-4567</span>
-                    <i class="far fa-envelope text-color-primary ml-4"></i><a href="mailto:mail@example.com" class="opacity-7 pl-2 text-color-light">mail@example.com</a>
-                </p>
             </div>
         </div>
     </div>
-    <div class="footer-copyright footer-copyright-style-2 bg-transparent footer-top-light-border">
-        <div class="container py-2">
-            <div class="row py-4">
-                <div class="col d-flex align-items-center justify-content-center mb-4 mb-lg-0">
-                    <p>© Copyright 2021. All Rights Reserved.</p>
+    <div class="container">
+        <div class="footer-copyright footer-copyright-style-2 pb-4">
+            <div class="py-2">
+                <div class="row py-4">
+                    <div class="col d-flex align-items-center justify-content-center mb-4 mb-lg-0">
+                        <p>© Copyright 2021. All Rights Reserved.</p>
+                    </div>
                 </div>
             </div>
         </div>
