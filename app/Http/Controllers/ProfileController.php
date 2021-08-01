2d5b5de54799c4a8e045140387f56e2e2561bf7e
Richieid23 <fitrarizki23@gmail.com>
2021-08-01 16:02:57 +0700

    fixing profile controller

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -15,4 +15,9 @@ class ProfileController extends Controller
     {
         return view('pages.profile.visi-misi');
     }
+
+    public function perangkat()
+    {
+        return view('pages.profile.perangkat');
+    }
 }
